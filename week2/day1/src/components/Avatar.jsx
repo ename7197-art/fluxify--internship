a2f66b4171c4496166a47864c246d3d5413efe3c
Emmyh <ename7197@gmail.com>
2026-04-03 17:05:49 +0200

    Delete week2/day1 directory

--- week2/day1/src/components/Avatar.jsx
+++
@@ -1,9 +0,0 @@
-export default function Avatar({ imageUrl, name }) {
-  return (
-    <img 
-      src={imageUrl} 
-      alt={name} 
-      style={{ width: '80px', height: '80px', borderRadius: '50%', objectFit: 'cover' }} 
-    />
-  );
-}
